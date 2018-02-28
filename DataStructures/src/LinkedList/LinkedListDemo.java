e1c3e0cdb9bd2ebf64865c62532d3d1dfcb9c8c3
Mohammed <work@momo.com>
2018-02-28 14:56:07 +0530

    Added isPalindrome Methods
    Geeks :-
    https://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/

--- DataStructures/src/LinkedList/LinkedListDemo.java
+++ DataStructures/src/LinkedList/LinkedListDemo.java
@@ -4,6 +4,9 @@ public class LinkedListDemo {
 
 	ListNode head;
 	ListNode headTwo;
+	ListNode slowPointPali;
+	ListNode fastPointPali;
+	ListNode reverseCheck;
 	static LinkedListDemo demo;
 
 	static class ListNode {
@@ -16,8 +19,8 @@ public class LinkedListDemo {
 		}
 	}
 
-	public void findCount(ListNode n) {
-		//ListNode n = head;
+	public int findCount(ListNode n) {
+		// ListNode n = head;
 		int listCount = 0;
 		while (n != null) {
 			// System.out.print(n.val+"------>");
@@ -25,11 +28,11 @@ public class LinkedListDemo {
 			listCount++;
 
 		}
-		System.out.println("Count of the list is---->" + listCount);
+		return listCount;
 	}
 
 	public void traversalList(ListNode n) {
-	//	ListNode n = head;
+		// ListNode n = head;
 		while (n != null) {
 			System.out.print(n.val + "------>");
 			n = n.next;
@@ -53,6 +56,20 @@ public class LinkedListDemo {
 
 	public ListNode reversUtil(ListNode curr, ListNode prev) {
 		if (curr.next == null) {
+			demo.reverseCheck = curr;
+			curr.next = prev;
+			return null;
+		} else {
+			ListNode next = curr.next;
+			curr.next = prev;
+			reversUtil(next, curr);
+
+		}
+		return null;
+	}
+
+	public ListNode reversUtilPali(ListNode curr, ListNode prev) {
+		if (curr.next == null) {
 			demo.head = curr;
 			curr.next = prev;
 			return null;
@@ -65,11 +82,28 @@ public class LinkedListDemo {
 		return null;
 	}
 
+	public ListNode returnSlowFastPalindrome(ListNode curr) {
+		slowPointPali = curr;
+		ListNode slowcut = slowPointPali;
+		fastPointPali = slowPointPali;
+
+		while (fastPointPali != null && fastPointPali.next != null) {
+			slowcut = slowPointPali;
+			slowPointPali = slowPointPali.next;
+			fastPointPali = fastPointPali.next.next;
+
+		}
+
+		slowcut.next = null;
+		return slowPointPali;
+
+	}
+
 	// Floyd Cycle Detection Algorithm.
-	public void detectLoop(ListNode slowPoint,ListNode fastPoint) {
-		//ListNode slowPoint = demo.head;
-		//ListNode fastPoint = demo.head;
-		
+	public void detectLoop(ListNode slowPoint, ListNode fastPoint) {
+		// ListNode slowPoint = demo.head;
+		// ListNode fastPoint = demo.head;
+
 		while (slowPoint != null && fastPoint != null && fastPoint.next != null) {
 			slowPoint = slowPoint.next;
 			fastPoint = fastPoint.next.next;
@@ -84,76 +118,139 @@ public class LinkedListDemo {
 		return;
 
 	}
-	
-	
-	public ListNode mergeSortedList(ListNode a,ListNode b)
-	{
+
+	public ListNode mergeSortedList(ListNode a, ListNode b) {
 		ListNode result = null;
-		if(a==null)
+		if (a == null)
 			return b;
-		if(b==null)
+		if (b == null)
 			return a;
-		if(a.val <= b.val){
+		if (a.val <= b.val) {
 			result = a;
-			result.next = mergeSortedList(a.next,b);
-			}
-		else{
+			result.next = mergeSortedList(a.next, b);
+		} else {
 			result = b;
-			
-			result.next=mergeSortedList(a,b.next);
+
+			result.next = mergeSortedList(a, b.next);
 		}
-			
+
 		return result;
-		
+
 	}
-	
-	
-	
+
+	public void isPalindrome(ListNode check) {
+		boolean isEven = (this.findCount(check) % 2 == 0 ? true : false);
+		boolean isPalindrome = true;
+		ListNode tempNode;
+		ListNode firstPoint, secondPoint;
+
+		// System.out.println("Middle element is"+(this.returnSlowFastPalindrome(check)).val);
+		this.returnSlowFastPalindrome(check);
+		if (isEven) {
+			// TODO for even block
+
+			// Reverse the second half;
+
+			this.reversUtilPali(demo.slowPointPali, null);
+			firstPoint = demo.head;
+			secondPoint = demo.reverseCheck;
+
+			while (firstPoint != null && secondPoint != null) {
+				if (firstPoint.val != secondPoint.val) {
+					isPalindrome = false;
+					break;
+				} else {
+					firstPoint = firstPoint.next;
+					secondPoint = secondPoint.next;
+				}
+
+			}
+
+			System.out.println("The palindrome result is " + isPalindrome);
+
+		} else {
+			// TODO for odd block
+			tempNode = demo.slowPointPali;
+			demo.slowPointPali = demo.slowPointPali.next;
+			tempNode.next = null;
+			this.reversUtilPali(demo.slowPointPali, null);
+
+			firstPoint = demo.headTwo;
+			secondPoint = demo.reverseCheck;
+
+			while (firstPoint != null && secondPoint != null) {
+				if (firstPoint.val != secondPoint.val) {
+					isPalindrome = false;
+					break;
+				} else {
+					firstPoint = firstPoint.next;
+					secondPoint = secondPoint.next;
+				}
+
+			}
+
+			System.out.println("The palindrome result is " + isPalindrome);
+
+		}
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		demo = new LinkedListDemo();
-		demo.head = new ListNode(10);
-		ListNode second = new ListNode(12);
-		ListNode third = new ListNode(13);
-		ListNode forth = new ListNode(14);
-		ListNode fifth = new ListNode(15);
+		demo.head = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode third = new ListNode(2);
+		ListNode forth = new ListNode(1);
+		// ListNode fifth = new ListNode(15);
 		// linking part:-
 
 		demo.head.next = second;
 		second.next = third;
 		third.next = forth;
-		forth.next = fifth;
-		fifth.next = null;
-		
-		
+		// forth.next = fifth;
+		forth.next = null;
+		// fifth.next = null;
+
 		// Second list
-		
-		demo.headTwo =  new ListNode(3);
-		ListNode secondTwo = new ListNode(5);
-		ListNode thirdTwo = new ListNode(7);
-		ListNode forthTwo = new ListNode(8);
-		ListNode fifthTwo = new ListNode(10);
-		
+
+		demo.headTwo = new ListNode(1);
+		ListNode secondTwo = new ListNode(2);
+		ListNode thirdTwo = new ListNode(1);
+		ListNode forthTwo = new ListNode(2);
+		ListNode fifthTwo = new ListNode(1);
+
 		demo.headTwo.next = secondTwo;
 		secondTwo.next = thirdTwo;
 		thirdTwo.next = forthTwo;
 		forthTwo.next = fifthTwo;
 		fifthTwo.next = null;
-		
+
 		// Test Traversal
-		 demo.traversalList(demo.head);
-		 System.out.println(" ");
-		 demo.traversalList(demo.headTwo);
-		 
-		 demo.mergeSortedList(demo.head,demo.headTwo);
-		 
-		 // To decide which node will become the newhead node;
-		 
-		 ListNode newhead = ((demo.head.val<=demo.headTwo.val)? demo.head:demo.headTwo);
-				 
-		 demo.traversalList(newhead);
-		//demo.detectLoop();
+		demo.traversalList(demo.head);
+		// System.out.println(" ");
+		// demo.traversalList(demo.headTwo);
+
+		demo.isPalindrome(demo.head);
+
+		System.out.println("After reversal printing the First half");
+
+		demo.traversalList(demo.head);
+
+		System.out.println("After reversal printing the second half");
+
+		demo.traversalList(demo.reverseCheck);
+
+		// demo.mergeSortedList(demo.head,demo.headTwo);
+
+		// To decide which node will become the newhead node;
+
+		// ListNode newhead = ((demo.head.val<=demo.headTwo.val)?
+		// demo.head:demo.headTwo);
+
+		// demo.traversalList(newhead);
+
+		// demo.detectLoop();
 
 		// Test FindCount
 		// demo.findCount();
@@ -165,7 +262,7 @@ public class LinkedListDemo {
 		// System.out.println("After revers in recursion " );
 
 		// demo.traversalList();
-		 
+
 	}
 
 }
