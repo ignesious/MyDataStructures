dfc1cd744afe914ea9f794dfba86b173b28648b1
moham <moham@DESKTOP-KDNN526>
2018-08-04 18:44:24 +0530

    Saturday Heap Vilayatu

---
+++ DataStructures/src/array/KLargestElement.java
@@ -0,0 +1,61 @@
+package array;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+ * https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+ */
+
+class pqsort implements Comparator<Integer> {
+	@Override
+	public int compare(Integer o1, Integer o2) {
+
+		return o1 - o2;
+	}
+
+}
+
+public class KLargestElement {
+
+	private void constructHeap(PriorityQueue<Integer> q, int input[]) {
+		// Looping input Array
+
+		for (int in : input) {
+			q.add(new Integer(in));
+		}
+
+	}
+
+	private int extractkmin(PriorityQueue<Integer> q, int k)
+
+	{
+
+		for (int i = 1; i <= k; i++) {
+
+			System.out.println("Peeking the head" + q.peek());
+			System.out.println("Polling the head" + q.poll());
+
+		}
+
+		return 0;
+
+	}
+
+	private int findKLargestElement(int input[], int k) {
+		PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(new pqsort());
+		constructHeap(pQueue, input);
+
+		System.out.println("Elements of priority Queue--->" + pQueue);
+		extractkmin(pQueue, k);
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		KLargestElement klarge = new KLargestElement();
+		int a[] = { -1, 1, 1, 3, 4, 7, 6, 9, 8 };
+		klarge.findKLargestElement(a, 2);
+	}
+
+}
---
+++ DataStructures/src/array/PositiveNegetive.java
@@ -0,0 +1,49 @@
+package array;
+/*
+ * https://algorithms.tutorialhorizon.com/rearrange-positive-and-negative-numbers-of-array-on-each-side-in-onlogn/
+ */
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class pqsort1 implements Comparator<Integer> {
+	@Override
+	public int compare(Integer o1, Integer o2) {
+
+		return o2 - o1;
+	}
+
+}
+
+public class PositiveNegetive {
+
+	public void heapify(PriorityQueue<Integer> q, int input[]) {
+		for (int i : input) {
+			q.add(new Integer(i));
+		}
+	}
+
+	public void seperatePositiveNegetive(int input[]) {
+		PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(
+				new pqsort1());
+
+		heapify(pQueue, input);
+		ArrayList<Integer> finalArray = new ArrayList<Integer>();
+		// Seperation
+
+		while (pQueue.peek() != null) {
+			finalArray.add(pQueue.poll());
+		}
+
+		System.out.println("Final Array is" + finalArray);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PositiveNegetive positiveNegetive = new PositiveNegetive();
+		int input[] = { 1, -2, -3, 4, 5, -2, -6 };
+		positiveNegetive.seperatePositiveNegetive(input);
+	}
+
+}
