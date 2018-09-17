f1a86f860074c7256cf3f7a9311500e4095d9fd8
Mohammed <work@momo.com>
2018-09-17 15:16:07 +0530

    Hibernate Criteria Query

--- DataStructures/src/hibernate/HibernateTestSimple.java
+++ DataStructures/src/hibernate/HibernateTestSimple.java
@@ -2,9 +2,16 @@ package hibernate;
 import java.util.Date;
 
 
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 public class HibernateTestSimple {
 
@@ -19,7 +26,47 @@ public class HibernateTestSimple {
 		Session session=sessionFactory.openSession();
 		session.beginTransaction();
 		
-		 userDetailsobj  = (UserDetails2)session.get(UserDetails2.class, 1);
+		// userDetailsobj  = (UserDetails2)session.get(UserDetails2.class, 1);
+		
+		
+	/*	//Creation Part
+		for(int i= 1 ;i<11;i++)
+		{
+			userDetailsobj =  new UserDetails2();
+		    userDetailsobj.setUserName("User is "+ i);
+		    session.save(userDetailsobj);
+		    
+		}*/
+		
+		
+//		Query part
+//		Query<UserDetails2> query=session.getNamedQuery("UserDetails2.byID");
+//		query.setInteger("userId",1);
+//		List<UserDetails2> resultList=query.list();
+		
+		
+		//Using Criteria API's
+		
+		
+		Criteria criteria=session.createCriteria(UserDetails2.class);
+		
+		Criterion name = Restrictions.gt("userid", 1);
+		Criterion name2 = Restrictions.eq("userid", 1);
+		Criterion finalCriterion=Restrictions.or(name, name2);
+		criteria.add(finalCriterion);
+		List<UserDetails2> resultList=criteria.list();
+		
+		
+		System.out.println(resultList);
+		for(UserDetails2 a:resultList)
+		{
+			System.out.println(a.getUserid() + " ------>"+ a.getUserName());
+		}
+		
+		
+		
+		
+		
 		//userDetailsobj.setUserName("Updated by mohammed");
 	//	session.save(vehicleDetails2);
 	//	session.save(userDetails1);
@@ -27,14 +74,18 @@ public class HibernateTestSimple {
 		session.close();
 		
 		//Updation Part now its detached object
-		userDetailsobj.setUserName("Updated Username");
-        System.out.println("Before persisting the detached object the user id is "+ userDetailsobj.getUserid());
+		
+		
+		
+		
+		
+       /* System.out.println("Before persisting the detached object the user id is "+ userDetailsobj.getUserid());
         
 		session=sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(userDetailsobj);
 		session.getTransaction().commit();
-		session.close();
+		session.close();*/
 		
 	}
 
--- DataStructures/src/hibernate/UserDetails2.java
+++ DataStructures/src/hibernate/UserDetails2.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQuery(name ="UserDetails2.byID", query="from UserDetails2 where userid = :userId")
 public class UserDetails2 {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
