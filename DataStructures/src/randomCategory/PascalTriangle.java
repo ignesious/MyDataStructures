40e6d23a3eb7db1a738da0df4fbe957b971bf0c0
moham <moham@DESKTOP-KDNN526>
2018-09-19 22:59:13 +0530

    Pascal Bhai

---
+++ DataStructures/src/randomCategory/PascalTriangle.java
@@ -0,0 +1,34 @@
+package randomCategory;
+
+/*
+ * https://www.geeksforgeeks.org/pascal-triangle/
+ * https://www.google.co.in/search?q=hockey+stick+pascal&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiV96uhycfdAhVkxosKHXxLCT4Q_AUIDigB#imgrc=N7OFUyD7kh4BBM:
+ */
+
+
+public class PascalTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 20;
+		long matter[][] = new long[n][n];
+
+		for (int line = 0; line < n; line++) {
+
+			for (int elem = 0; elem <= line; elem++) {
+
+				if (elem == 0 || elem == line) {
+					matter[line][elem] = 1;
+					System.out.print(matter[line][elem] + "\t");
+				} else {
+					matter[line][elem] = matter[line - 1][elem - 1]
+							+ matter[line - 1][elem];
+					System.out.print(matter[line][elem] + "\t");
+				}
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
